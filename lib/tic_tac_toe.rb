a1f5207bc339121c4746d163eb8541f1e7a3faca
Victor Torres <torres.victor136@gmail.com>
2020-07-14 16:20:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,3 +48,91 @@ def turn
   else
     turn
   end 
+
+  def turn_count
+    count = 0 
+    @board.each |index|
+    if index == "X" || index == "O"
+      turn += 1 
+    end 
+  end 
+  return count 
+end 
+
+def turn_count
+  turns =  turn_count
+  if turns % 2 == 0 
+    player = "X"
+  else 
+    player = "O"
+  end 
+  return player 
+end 
+
+def won?
+  WIN_COMBINATIONS.each {|win_combination|
+    index_0 = win_combination[0]
+    index_1 = win_combination[1]
+    index_2 = win_combination[2]
+
+    position_1 = @board[index_0]
+    position_2 = @board[index_1]
+    position_3 = @board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+else 
+  return false
+end
+
+def full?
+  @board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?
+  if !won? && full?
+    return true
+  else
+    return false
+  end
+end
+
+def over?
+  if won? || draw?
+    return true
+  else
+    return false
+  end
+end
+
+def winner
+  index = []
+  index = won?
+  if index == false
+    return nil
+  else
+    if @board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
+
+def play
+  until over? == true
+    turn
+  end
+
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
+
+end
